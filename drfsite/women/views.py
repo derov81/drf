ee096b5fd702bc23ed12a1e90778a1424b593034
derov81 <maniprutby@gmail.com>
2025-07-10 23:08:43 +0300

    Viewsets и ModelViewSet

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,5 +1,5 @@
 from django.forms import model_to_dict
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,18 +7,22 @@ from rest_framework.views import APIView
 from .models import Women
 from .serializers import WomenSerializer
 
-
-class WomenAPIList(generics.ListCreateAPIView):
+class WomenVieSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
-class WomenAPIUpdate(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
 
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
 
 # class WomenAPIView(APIView):
